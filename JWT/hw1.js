378f9be4428f9089725e47d08a924b0fbc725361
Polly2518 <77542142+Polly2518@users.noreply.github.com>
2022-05-11 22:37:30 +0700

    ีupdate 7

--- JWT/hw1.js
+++ JWT/hw1.js
@@ -1,40 +1,115 @@
 require('dotenv').config()
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser')
+const SHA256 = require("crypto-js/sha256");
 const app = express();
 app.use(express.json());
+app.use(cookieParser())
 
 const users = []
-const testuseracc = [{
-    username: 'cap',
-    title: 'access1'
 
-}, {
-    username: 'cap2',
-    title: 'access2'
-}]
-app.get('/user/getinfo', (req, res) => {
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/login.html")
+});
 
-    res.status(200).send(users)
+app.get('/home', (req, res) => {
+    if (!req.cookies.token) return res.redirect('/error')
+
+    jwt.verify(req.cookies.token, process.env.ACCESS_TOKEN_SECRET, (err, result) => {
+        if (err) {
+            return res.redirect('/error')
+        }
+        res.sendFile(__dirname + "/home.html")
+    })
+});
+
+app.get('/error', (req, res) => {
+    res.sendFile(__dirname + "/error.html")
+});
+
+app.post('/login', (req, res) => {
+    if (req.body.username == users.username && req.body.password == users.password) {
+        const token = jwt.sign({ username: req.body.username }, process.env.ACCESS_TOKEN_SECRET)
+        res.cookie('token', token)
+        res.redirect('/home')
+    } else {
+        res.cookie('token', "")
+        res.redirect('/')
+    }
+})
+
+app.get('/register', (req, res) => {
+
+    res.sendFile(__dirname + '/regis.html')
+
 
 })
-app.post('/user/create', async(req, res) => {
-
+app.post('/user/register', (req, res) => {
     try {
 
-        const hashpassword = await bcrypt.hash(req.body.password, 10)
+        const hashpassword = SHA256(req.body.password)
         const user = { username: req.body.username, password: hashpassword }
         users.push(user)
-        res.status(201).send('user created.')
-
+        console.log('user registered')
+        res.sendFile(__dirname + '/login.html')
     } catch (error) {
         res.status(500).send('Not Success ' + error)
     }
 
 })
 
-app.post('/user/login', async(req, res) => {
+app.listen(3000, () => {
+    console.log('Listening on port: 3000');
+})
+
+
+
+
+
+
+
+/*app.get('/user/getinfo', (req, res) => {
+
+    res.status(200).send(users)
+
+})
+
+app.get('/home', authToken, (req, res) => {
+
+    res.sendFile(__dirname + '/home.html')
+
+
+})
+
+app.get('/', (req, res) => {
+
+    res.sendFile(__dirname + '/login.html')
+
+
+})
+
+app.get('/register', (req, res) => {
+
+    res.sendFile(__dirname + '/regis.html')
+
+
+})
+app.post('/user/register', (req, res) => {
+    try {
+
+        const hashpassword = SHA256(req.body.password)
+        const user = { username: req.body.username, password: hashpassword }
+        users.push(user)
+        console.log('user registered')
+        res.sendFile(__dirname + '/login.html')
+    } catch (error) {
+        res.status(500).send('Not Success ' + error)
+    }
+
+})
+
+app.post('/user/login', authToken, async(req, res) => {
     const user = users.find(user => users.username = req.body.username)
     if (user == null) {
 
@@ -45,7 +120,10 @@ app.post('/user/login', async(req, res) => {
             const username = req.body.username
             const userath = { username: username }
             const accesstoken = jwt.sign(userath, process.env.ACCESS_TOKEN_SECRET)
-            res.json({ accesstoken: accesstoken })
+
+            console.log({ accesstoken: accesstoken })
+            res.cookie(accesstoken.toString())
+            res.redirect('/home')
 
         } else {
             res.status(400).send('Password worng or Empty')
@@ -59,16 +137,11 @@ app.post('/user/login', async(req, res) => {
 
 })
 
-app.post('/user/getacc', authToken, (req, res) => {
-    res.json(testuseracc.filter(testuseracc => testuseracc.username == req.userath.username))
-
-
-})
 
 function authToken(req, res, next) {
 
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
+
+    const token = req.cookie.accesstoken
     if (token == null) return res.status(401).send("NO TOKEN FOUND")
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, userath) => {
@@ -77,8 +150,4 @@ function authToken(req, res, next) {
         next()
     })
 
-}
-
-app.listen(3000, () => {
-    console.log('Listening on port: 3000');
-})
+}*/
