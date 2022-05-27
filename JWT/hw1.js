98d5816c1d88bf31ab11ed88b07185f8916c3ba7
Polly2518 <77542142+Polly2518@users.noreply.github.com>
2022-05-27 13:57:26 +0700

    update 8

--- JWT/hw1.js
+++ JWT/hw1.js
@@ -4,9 +4,11 @@ const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser')
 const SHA256 = require("crypto-js/sha256");
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser())
 app.use(express.urlencoded({ extended: true }))
+
 const users = []
 
 app.get('/', (req, res) => {
@@ -15,15 +17,22 @@ app.get('/', (req, res) => {
 
 
 app.post('/login', (req, res) => {
-    console.log(req.body)
+    let check = 0;
     if (!req.body.username || !req.body.password) {
-        return res.status(401).send("PLEASE INPUT DATA")
+        return res.status(401).send("Error : invalid data")
     }
-    if (req.body.username == users.username && req.body.password == users.password) {
-        const token = jwt.sign({ username: req.body.username }, process.env.ACCESS_TOKEN_SECRET)
-        res.cookie('token', token)
+    for (let i = 0; i < users.length; i++) {
+        if (req.body.username == users[i]["username"] || req.body.password == users[i]["password"]) {
+            const token = jwt.sign({ username: req.body.username }, process.env.ACCESS_TOKEN_SECRET)
+            res.cookie('token', token)
+            check++;
+        }
+    }
+    if (check == 1) {
+        console.log("login success")
         res.redirect('/home')
     } else {
+        console.log("์Wrong data")
         res.cookie('token', "")
         res.redirect('/')
     }
@@ -31,46 +40,38 @@ app.post('/login', (req, res) => {
 
 
 app.get('/register', (req, res) => {
-
     res.sendFile(__dirname + '/regis.html')
-
-
 })
+
 app.post('/user/register', (req, res) => {
     console.log(req.body.username)
     if (!req.body.username || !req.body.password) {
-        return res.status(401).send("PLEASE INPUT DATA")
+        return res.status(401).send("Error : invalid data")
     }
     try {
 
         const hashpassword = SHA256(req.body.password).toString()
         const user = { username: req.body.username, password: hashpassword }
-        users.push(req.body.username)
-        console.log('user registered')
-        console.log(users)
+        users.push(user)
+        console.log("user registered")
         res.sendFile(__dirname + '/login.html')
     } catch (error) {
-        res.status(500).send('Not Success ' + error)
+        res.status(500).send("Not Success " + error)
     }
 
 })
 
 
 app.get('/home', (req, res) => {
-    if (!req.body.username || !req.body.password) {
-        return res.status(401).send("PLEASE INPUT DATA")
-    }
     if (!req.cookies.token) return res.sendFile(__dirname + '/login.html')
 
     jwt.verify(req.cookies.token, process.env.ACCESS_TOKEN_SECRET, (err, result) => {
         if (err) {
             return res.sendFile(__dirname + '/login.html')
         }
-        res.sendFile(__dirname + "/home.html")
+        res.sendFile(__dirname + '/home.html')
     })
 });
-
-
 
 
 app.listen(3000, () => {
